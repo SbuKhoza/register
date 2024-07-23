39025f8991a20607997eb0aa1d6f85d00eac3472
SbuKhoza <sibusisok59@gmail.com>
2024-07-23 12:14:51 +0200

    update

---
+++ .history/src/axios_20240723105207.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+export const getPosts = async () => {
+    const response = await AudioParam.get('posts')
+}
---
+++ .history/src/axios_20240723105216.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+
+export const getPosts = async () => {
+    const response = await AudioParam.get('posts')
+    
+}
